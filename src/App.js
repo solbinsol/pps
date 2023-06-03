9bf84c0b171219a2ed6c985aa2a6f32322a3b429
solbinsol <qkaejwnj@naver.com>
2023-06-03 20:12:45 +0900

    ㄴㄴ

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import PPS from './PPS';
 
+import { BrowserRouter, Router ,Route,Routes} from 'react-router-dom';
+import DetailView from "./Detail"
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PPS />} />
+          <Route path="/detail" element={<DetailView />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   
         <React.StrictMode>
          
-              <PPS />
+            <App></App>
 
 
 
