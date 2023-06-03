9b13d23039d62118e0ee8b9143347a013afc3170
solbinsol <qkaejwnj@naver.com>
2023-06-04 03:12:08 +0900

    s

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import PPS from './PPS';
-import { BrowserRouter, Router ,Route} from 'react-router-dom';
-import DetailView from "./Detail"
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
