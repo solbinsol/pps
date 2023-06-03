1495d0028fb439921c5c174bc873b02eb87a212f
solbinsol <qkaejwnj@naver.com>
2023-06-03 20:12:38 +0900

    상세페이지

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -3,8 +3,20 @@ import React from "react";
 const Detail = () =>{
 
     return(
-        <div>
-            ss
+        <div className="ReedMe">
+            <div className="header">
+                <h1 className="logo">KSB's      ReedME!</h1>
+                <ul className="Menu">
+                    <li>About me</li>
+                    <li>Skills</li>
+                    <li>Archiving</li>
+                    <li>Projects</li>
+                </ul>
+            </div>
+            <div>
+                
+            </div>
+
         </div>
     )
 } 
